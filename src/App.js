c27acd6ec887b0dccac8dfad555175af08040594
mubquaidian <66357704+mubquaidian@users.noreply.github.com>
2020-11-19 13:02:41 -0500

    New test repository

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Courses from './courses.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello Programmers of PIAIC</h1>
+        <hr color='white'/> 
+        <h5><Courses courseName="AIC"/></h5> 
+        <h5><Courses courseName="IOT"/></h5> 
+        <h5><Courses courseName="BC"/></h5> 
+        </header>
     </div>
   );
 }
---
+++ src/courses.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Courses(props){
+    return(
+    <h1>PIAIC-Courses are {props.courseName}</h1>        
+    );
+}
+export default Courses;
