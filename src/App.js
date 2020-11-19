bc8546bc662e33e092c49f724aefd7e449cd7b37
mubquaidian <66357704+mubquaidian@users.noreply.github.com>
2020-11-19 16:06:48 -0500

    Final testing for auto deployment on surge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Courses from './courses.js';
+import Dinner from './Dinner';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <h1>Hello Programmers of PIAIC</h1>
         <hr color='white'/> 
         <h5><Courses courseName="AIC & BC"/></h5> 
+        <h7><Dinner cooking="pasta"/></h7>
         </header>
     </div>
   );
